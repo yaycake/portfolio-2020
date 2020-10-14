214616431eef544330fb9380426f2b273b84342b
Grace <yaycake@gmail.com>
2020-10-14 17:26:01 +0800

    success graphql query; first draft styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
-
+import Img from "gatsby-image"; 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
@@ -14,11 +14,37 @@ import Section from "../components/Section"
 
 const IndexPage = (props) => {
 
-  // const data = useStaticQuery(graphql`
-  //   query { 
-  //     allMdx (filter: )
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query { 
+      allMdx(filter: {frontmatter: {featured_: {eq: true}}}) {
+        edges {
+          node {
+            frontmatter {
+              path
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              category
+              skills
+              tools
+              description
+              featured_
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const featured = data.allMdx.edges; 
+
+  console.log(featured)
+
   const siteTitle = "Grace | Design & Code"
 
   return (
@@ -58,9 +84,43 @@ const IndexPage = (props) => {
         </div>
         
         <div className={styles.index_projects__grid}>
-          <div className={styles.index_projects__grid__project1}></div>
-          <div className={styles.index_projects__grid__project2}></div>
-          <div className={styles.index_projects__grid__project3}></div>
+          <div className={styles.index_projects__grid__project1}>
+            <Link to={featured[0].node.frontmatter.path}>
+              <Img className={styles.projects__grid__img} fluid={featured[0].node.frontmatter.featuredImage.childImageSharp.fluid}/>
+              <div className={styles.projects__grid__content}>
+                
+                  <span className={styles.projects__grid__title}>{featured[0].node.frontmatter.title} </span>
+                  <span>{featured[0].node.frontmatter.category}</span>
+                  <span> &#8594;</span>
+              </div>
+            </Link> 
+          </div>
+          
+              
+          <div className={styles.index_projects__grid__project2}>
+            <Link to={featured[1].node.frontmatter.path}>
+                <Img fluid={featured[1].node.frontmatter.featuredImage.childImageSharp.fluid}/>
+                <div className={styles.projects__grid__content}>
+                  
+                  <span className={styles.projects__grid__title}>{featured[1].node.frontmatter.title} </span>
+                  <span>{featured[1].node.frontmatter.category}</span>
+                  <span> &#8594;</span>
+                </div>
+              </Link>
+          </div>
+        
+          <div className={styles.index_projects__grid__project3}>
+            <Link to={featured[2].node.frontmatter.path}>
+              <Img fluid={featured[2].node.frontmatter.featuredImage.childImageSharp.fluid}/>
+              <div className={styles.projects__grid__content}>
+                
+                <span className={styles.projects__grid__title}>{featured[2].node.frontmatter.title} </span>
+                <span>{featured[2].node.frontmatter.category}</span>
+                <span> &#8594;</span>
+              </div>
+            </Link>
+
+          </div>
         </div>
 
         <h4 className={styles.index_projects_action_mobile}> <Link to="/projects">View All Projects &#8594;</Link></h4>
