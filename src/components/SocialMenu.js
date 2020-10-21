7a1921ca91e647da967c00768415596e3d724571
Grace <yaycake@gmail.com>
2020-10-21 16:44:15 +0800

    try using static for social files

--- src/components/SocialMenu.js
+++ src/components/SocialMenu.js
@@ -4,23 +4,50 @@ import styled from "styled-components"
 import styles from './SocialMenu.module.scss'
 import CuteNavLink from "./cuteNavLink"
 
-import Github from '../images/icons/github-48.png'
-import Resume from '../images/icons/resume-48.png'
-import Linkedin from '../images/icons/linkedin-48.png'
-import Dev from '../images/icons/dev.svg'
+// import Github from '../images/icons/github-48.png'
+// import Resume from '../images/icons/resume-48.png'
+// import Linkedin from '../images/icons/linkedin-48.png'
+// import Dev from '../images/icons/dev.svg'
 
 const SocialMenu = () => {
+    console.log(Github)
+    console.log(Resume)
+    console.log(Linkedin)
+    console.log(Dev)
+
+
+    console.log(typeof Github)
+    console.log(typeof Resume)
+    console.log(typeof Linkedin)
+    console.log(typeof Dev)
     return (
         <ul className={styles.socialMenu}>
             <li className={styles.socialMenu__item}>
-                <a aria-label="Grace's Github" href="https://github.com/yaycake" title="Grace's Github"><img className={styles.social_icons} src={Github}></img></a>
+                <a aria-label="Grace's Github" href="https://github.com/yaycake" title="Grace's Github">
+                    <img 
+                    className={styles.social_icons} 
+                    // src={Github}
+                    src = "../github-48.png"
+                    ></img>
+                </a>
             </li>
             <li className={styles.socialMenu__item}>
-                <a aria-label="Grace's LinkedIn Profile" href="https://www.linkedin.com/in/ygrace/" title="Grace's LinkedIn"><img className={styles.social_icons} src={Linkedin}></img></a>
+                <a aria-label="Grace's LinkedIn Profile" 
+                    href="https://www.linkedin.com/in/ygrace/" 
+                    title="Grace's LinkedIn">
+                        <img className={styles.social_icons} 
+                        // src={Linkedin}
+                        src="../linkedin-48.png"
+                        ></img>
+                </a>
             </li>
             <li className={styles.socialMenu__item}> 
-                <a aria-label="Grace'sDev.to Blog" href="https://dev.to/yaycake">
-                    <img className={styles.social_icons} src={Dev}/>
+                <a aria-label="Grace'sDev.to Blog" 
+                    href="https://dev.to/yaycake">
+                    <img className={styles.social_icons} 
+                        // src={Dev}
+                        src= "../dev.svg"
+                        />
                 </a>
             </li>
             <li className={styles.socialMenu__item}><Link aria-label="Grace's Resume" to="/resume"><img className={styles.social_icons} src={Resume}></img></Link></li>
