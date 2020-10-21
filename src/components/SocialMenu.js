c1c3c4ab85de62041d32b0ec95a8f92c9226132f
Grace <yaycake@gmail.com>
2020-10-21 19:07:05 +0800

    imported assets to social menu lowercase

--- src/components/SocialMenu.js
+++ src/components/SocialMenu.js
@@ -4,30 +4,30 @@ import styled from "styled-components"
 import styles from './SocialMenu.module.scss'
 import CuteNavLink from "./cuteNavLink"
 
-// import Github from '../images/icons/github-48.png'
-// import Resume from '../images/icons/resume-48.png'
-// import Linkedin from '../images/icons/linkedin-48.png'
-// import Dev from '../images/icons/dev.svg'
+import github from '../images/icons/github-48.png'
+import resume from '../images/icons/resume-48.png'
+import linkedin from '../images/icons/linkedin-48.png'
+import dev from '../images/icons/dev.svg'
 
 const SocialMenu = () => {
-    // console.log(Github)
-    // console.log(Resume)
-    // console.log(Linkedin)
-    // console.log(Dev)
+    console.log(github)
+    console.log(resume)
+    console.log(linkedin)
+    console.log(dev)
 
 
-    // console.log(typeof Github)
-    // console.log(typeof Resume)
-    // console.log(typeof Linkedin)
-    // console.log(typeof Dev)
+    console.log(typeof github)
+    console.log(typeof resume)
+    console.log(typeof linkedin)
+    console.log(typeof dev)
     return (
         <ul className={styles.socialMenu}>
             <li className={styles.socialMenu__item}>
                 <a aria-label="Grace's Github" href="https://github.com/yaycake" title="Grace's Github">
                     <img 
                     className={styles.social_icons} 
-                    // src={Github}
-                    src = "../github-48.png"
+                    src={github}
+                    // src = "../github-48.png"
                     ></img>
                 </a>
             </li>
@@ -36,8 +36,8 @@ const SocialMenu = () => {
                     href="https://www.linkedin.com/in/ygrace/" 
                     title="Grace's LinkedIn">
                         <img className={styles.social_icons} 
-                        // src={Linkedin}
-                        src="../linkedin-48.png"
+                        src={linkedin}
+                        // src="../linkedin-48.png"
                         ></img>
                 </a>
             </li>
@@ -45,8 +45,8 @@ const SocialMenu = () => {
                 <a aria-label="Grace'sDev.to Blog" 
                     href="https://dev.to/yaycake">
                     <img className={styles.social_icons} 
-                        // src={Dev}
-                        src= "../dev.svg"
+                        src={dev}
+                        // src= "../dev.svg"
                         />
                 </a>
             </li>
@@ -55,8 +55,8 @@ const SocialMenu = () => {
                 to="/resume">
                     <img 
                         className={styles.social_icons} 
-                        // src={Resume}
-                        src = "../resume-48.png"
+                        src={resume}
+                        // src = "../resume-48.png"
                         ></img>
                 </Link>
             </li>
