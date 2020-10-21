0d9c69dbfe6b83ffde4178dfb08cde2f528ffc6d
Grace <yaycake@gmail.com>
2020-10-21 20:22:57 +0800

    added files to static/assets folder

--- src/components/SocialMenu.js
+++ src/components/SocialMenu.js
@@ -2,10 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from './SocialMenu.module.scss'
 
-import github from '../images/icons/github-48.png'
-import resume from '../images/icons/resume-48.png'
-import linkedin from '../images/icons/linkedin-48.png'
-import dev from '../images/icons/dev.svg'
+// import github from '../images/icons/github-48.png'
+// import resume from '../images/icons/resume-48.png'
+// import linkedin from '../images/icons/linkedin-48.png'
+// import dev from '../images/icons/dev.svg'
 
 // import github from '/github-48.png'
 // import resume from '/resume-48.png'
@@ -13,24 +13,24 @@ import dev from '../images/icons/dev.svg'
 // import dev from '/dev.svg'
 
 const SocialMenu = () => {
-    console.log(github)
-    console.log(resume)
-    console.log(linkedin)
-    console.log(dev)
+    // console.log(github)
+    // console.log(resume)
+    // console.log(linkedin)
+    // console.log(dev)
 
 
-    console.log(typeof github)
-    console.log(typeof resume)
-    console.log(typeof linkedin)
-    console.log(typeof dev)
+    // console.log(typeof github)
+    // console.log(typeof resume)
+    // console.log(typeof linkedin)
+    // console.log(typeof dev)
     return (
         <ul className={styles.socialMenu}>
             <li className={styles.socialMenu__item}>
                 <a aria-label="Grace's Github" href="https://github.com/yaycake" title="Grace's Github">
                     <img 
                     className={styles.social_icons} 
-                    src={github}
-                    // src = "../github-48.png"
+                    // src={github}
+                    src = "/github-48.png"
                     ></img>
                 </a>
             </li>
@@ -39,8 +39,8 @@ const SocialMenu = () => {
                     href="https://www.linkedin.com/in/ygrace/" 
                     title="Grace's LinkedIn">
                         <img className={styles.social_icons} 
-                        src={linkedin}
-                        // src="../linkedin-48.png"
+                        // src={linkedin}
+                        src="/linkedin-48.png"
                         ></img>
                 </a>
             </li>
@@ -48,8 +48,8 @@ const SocialMenu = () => {
                 <a aria-label="Grace'sDev.to Blog" 
                     href="https://dev.to/yaycake">
                     <img className={styles.social_icons} 
-                        src={dev}
-                        // src= "../dev.svg"
+                        // src={dev}
+                        src= "/dev.svg"
                         />
                 </a>
             </li>
@@ -58,8 +58,8 @@ const SocialMenu = () => {
                 to="/resume">
                     <img 
                         className={styles.social_icons} 
-                        src={resume}
-                        // src = "../resume-48.png"
+                        // src={resume}
+                        src = "/resume-48.png"
                         ></img>
                 </Link>
             </li>
