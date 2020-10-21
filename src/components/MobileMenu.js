c40cb0766aaf4f98cfe954af57228389eb19e94c
Grace <yaycake@gmail.com>
2020-10-21 15:39:51 +0800

    add aria labels to mobile menu and cuteNavLinks

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -9,25 +9,25 @@ const MobileMenu = ({showMenu, clicked, keyed}) => {
         <div className={styles.mobileMenu}
             style={ showMenu ? { display: `flex` } : { display: `none` }}>
 
-            <Link onClick={clicked} onKeyUp={keyed} to="/">
+            <Link aria-label="Go to home page" onClick={clicked} onKeyUp={keyed} to="/">
                 <span className={styles.linkLabel}>Go</span> 
                 Home
             </Link>
             
-            <Link onClick={clicked} onKeyUp={keyed} to="/projects">
+            <Link aria-label = "View My Projects" onClick={clicked} onKeyUp={keyed} to="/projects">
                 <span className={styles.linkLabel}>View My</span> 
                Projects
             </Link>
            
-            <Link onClick={clicked} onKeyUp={keyed} to="/projects">
+            <Link aria-label = "Read my blog" onClick={clicked} onKeyUp={keyed} to="/projects">
                 <span className={styles.linkLabel}>Read My</span> 
                 Blog
             </Link>
-            <Link onClick={clicked} onKeyUp={keyed} to="/about">
+            <Link aria-label = "About Grace" onClick={clicked} onKeyUp={keyed} to="/about">
                 <span className={styles.linkLabel}> Learn </span>
                 About Me
             </Link>
-            <Link onClick={clicked} onKeyUp={keyed} to="/contact">
+            <Link aria-label = "Contact Grace" onClick={clicked} onKeyUp={keyed} to="/contact">
                 <span className={styles.linkLabel}> Questions? </span>
                 Contact Me
             </Link>
